06be0ce51da545d4cc4c6e471b2a7185063a542b
Doo O <en13en57@gmail.com>
2021-12-13 21:21:11 +0900

    211213

---
+++ 211213/src/main/java/kr/green/maven/Java211213/Exam.java
@@ -0,0 +1,17 @@
+package kr.green.maven.Java211213;
+
+import java.util.Scanner;
+
+public class Exam {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c=0, length=0, temp=0;
+		for(int i=1;i<=b;i++) {
+			while(temp>=0) {
+				length *= 10;
+			}
+		}
+	}
+}
