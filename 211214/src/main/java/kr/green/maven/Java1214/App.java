7aa9e53db758298d961b32f7c7ad1cf97ac85d63
Doo O <en13en57@gmail.com>
2021-12-14 20:41:10 +0900

    211214

---
+++ 211214/src/main/java/kr/green/maven/Java1214/App.java
@@ -0,0 +1,13 @@
+package kr.green.maven.Java1214;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
